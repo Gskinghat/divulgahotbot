3255a87bf2a95a546317556f21c30cf0ddcb46c9
Gskinghat <guilhermegoldeletra1@gmail.com>
2025-05-15 16:02:11 -0300

    Update divulgahotbot.py

--- divulgahotbot.py
+++ divulgahotbot.py
@@ -16,7 +16,7 @@ import os
 nest_asyncio.apply()
 
 # === CONFIG ===
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")  # Substitua pelo token ou use variável de ambiente
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7664156068:AAEsh9NV-eYIP7i_Z12z8UsL6K_36cdLTBQ")  # Substitua pelo token ou use variável de ambiente
 ADMIN_ID = int(os.getenv("ADMIN_ID", 6835008287))  # Substitua ou use variável de ambiente
 
 # === FUNÇÕES ===
