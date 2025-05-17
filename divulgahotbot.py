51617781a980ac2e40da8614e132a599ac1f769b
Gskinghat <guilhermegoldeletra1@gmail.com>
2025-05-17 01:38:03 -0300

    Update divulgahotbot.py

--- divulgahotbot.py
+++ divulgahotbot.py
@@ -1,6 +1,7 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler  # Importando o AsyncIOScheduler
 import pytz
 import traceback
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
 # Definir o fuso horário de Brasília (GMT-3)
@@ -92,35 +93,4 @@ scheduler = AsyncIOScheduler()  # Agora o scheduler é inicializado corretamente
 # Main
 async def main():
     # Configuração do bot com pool e timeout ajustados
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    # Chama a função para criar a tabela 'canais' e 'views' se não existirem
-    create_tables()
-
-    # Chama a função para adicionar todos os canais novamente
-    adicionar_varios_canais()
-
-    # Ajustando o pool de conexões e o timeout com a API pública
-    app.bot._request_kwargs = {
-        'timeout': 30,  # Timeout de 30 segundos
-        'pool_size': 20  # Pool de conexões de 20
-    }
-
-    # Agendando as mensagens para horários específicos em horário de Brasília
-    try:
-        scheduler.add_job(enviar_mensagem_programada, "cron", hour=18, minute=0, args=[app.bot], timezone=brasilia_tz)  # 18h
-        scheduler.add_job(enviar_mensagem_programada, "cron", hour=22, minute=0, args=[app.bot], timezone=brasilia_tz)  # 22h
-        scheduler.add_job(enviar_mensagem_programada, "cron", hour=4, minute=0, args=[app.bot], timezone=brasilia_tz)   # 4h
-        scheduler.add_job(enviar_mensagem_programada, "cron", hour=11, minute=0, args=[app.bot], timezone=brasilia_tz)  # 11h
-        scheduler.start()  # Iniciando o scheduler
-    except Exception as e:
-        logger.error(f"Erro ao agendar tarefa: {e}")
-
-    print("✅ Bot rodando com polling e agendamento diário!")
-    await app.run_polling(drop_pending_updates=True)  # Apenas polling, sem webhook
-
-if __name__ == "__main__":
-    try:
-        asyncio.run(main())  # Usando asyncio.run diretamente
-    except Exception as e:
-        logger.error(f"Erro ao iniciar o bot: {e}")
