e55f4b15db1a4aa954e4544cd04b512e3e5383b5
Gskinghat <guilhermegoldeletra1@gmail.com>
2025-05-18 17:27:54 -0300

    Update divulgahotbot.py

--- divulgahotbot.py
+++ divulgahotbot.py
@@ -14,8 +14,8 @@ from telegram.ext import (
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import nest_asyncio
 import os
-import pytz
 from dotenv import load_dotenv
+import pytz
 
 # Configuração do logger
 logging.basicConfig(level=logging.INFO)
@@ -59,21 +59,6 @@ def create_tables():
     close_db_connection(conn)
 
 # Funções de persistência
-def get_views():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT total_views FROM views WHERE rowid = 1")
-    result = cursor.fetchone()
-    close_db_connection(conn)
-    return result[0] if result else 0
-
-def update_views(new_views):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("UPDATE views SET total_views = ? WHERE rowid = 1", (new_views,))
-    conn.commit()
-    close_db_connection(conn)
-
 def add_canal(chat_id):
     conn = get_db_connection()
     cursor = conn.cursor()
@@ -89,53 +74,53 @@ def get_canais():
     close_db_connection(conn)
     return canais
 
-# === FUNÇÕES ===
+# === ADICIONANDO SEUS CANAIS ===
 
-# Função para verificar os canais onde o bot é administrador
-async def verificar_admins(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    bot = context.bot
-    canais_verificados = []
+# Lista dos canais que o bot é administrador
+canal_ids = [
+    # fanny HOT cont
+    -1002649049975, -1002521735139, -1002649646963, -1002648991007, -1002566487140, -1002610733678,
+    -1002631072802, -1002342627563, -1002581311796, -1002645708556,
 
-    for canal in get_canais():  # Lista dos canais cadastrados
-        try:
-            membro = await bot.get_chat_member(canal[0], bot.id)
-            if membro.status in ["administrator", "creator"]:
-                canais_verificados.append(canal[0])
-        except Exception as e:
-            logger.error(f"Erro ao verificar {canal[0]}: {e}")
+    # fanny irmã conta
+    -1002261886788, -1002680847721, -1002663744586, -1002576716175, -1002422908996, -1002579739516,
+    -1002305906018, -1002608129630, -1002648451435, -1002632167498,
 
-    texto = f"✅ Bot é administrador em {len(canais_verificados)} canais públicos."
-    await update.message.reply_text(texto)
+    # GS
+    -1002634219030, -1002659272412, -1002532471834, -1002555455661, -1002694017662, -1002619113523,
+    -1002663654523, -1002532598032,
 
-# Função para obter o chat_id
-async def get_chat_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat_id = update.message.chat.id  # Obtém o chat_id do comando de start
-    await update.message.reply_text(f"Seu chat_id é: {chat_id}")
+    # CONTA 1 CHANEL
+    -1002569779659,
 
-# Função para adicionar canais via comando
-async def add_canal_comando(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Verificar se o comando foi enviado por um admin
-    if update.message.from_user.id != ADMIN_ID:
-        await update.message.reply_text("Você não tem permissão para adicionar canais.")
-        return
+    # CONTA GS MVP
+    -1002637058718, -1002673806655, -1002617005901, -1002591102891, -1002502547461, -1002527153879,
+    -1002547163724, -1002686248264, -1002549685600, -1002683098146,
 
-    # Verificar se foi fornecido um ID de canal
-    if not context.args:
-        await update.message.reply_text("Por favor, forneça o ID do canal para adicionar.")
-        return
+    # HOT 20
+    -1002521780775, -1002496248801, -1002652344851, -1002510129415, -1002524424215, -1002699745337,
+    -1002620495214, -1002620603496, -1002670501142, -1002293619562, -1002659153687, -1002506650062,
+    -1002689763350, -1002531772113, -1002674038291, -1002670668044, -1002673660530, -1002658512135,
+    -1002521019939, -1002370525614,
 
-    canal_id = context.args[0]  # O ID do canal será o primeiro argumento
+    # henrique tele
+    -1002534336418, -1002636065794, -1002592699953, -1002626812866, -1002507566931, -1002448809940,
+    -1002611400878, -1002674890916, -1002592636698, -1002581071012,
 
-    try:
-        canal_id = int(canal_id)  # Certificar-se de que o ID é um número inteiro
+    # ALE CONTA
+    -1002676023257, -1002555594530, -1002637517683, -1002614028594, -1002521671210, -1002563919969,
+    -1002320892399, -1002581354578, -1002535585069, -1002662161329
+]
+
+# Função para adicionar todos os canais ao banco de dados
+def adicionar_varios_canais():
+    for canal_id in canal_ids:
         add_canal(canal_id)
-        await update.message.reply_text(f"Canal {canal_id} adicionado com sucesso!")
-    except ValueError:
-        await update.message.reply_text("O ID do canal deve ser um número válido.")
+        logger.info(f"Canal {canal_id} adicionado ao banco de dados.")
 
 # Função para enviar a mensagem personalizada com a lista de canais
 async def enviar_mensagem_programada(bot):
-    logger.info("Iniciando envio de mensagens programadas...")  # Log para iniciar a tarefa
+    logger.info("Iniciando envio de mensagens programadas...")
 
     mensagem = (
         "💎: {𝗟 𝗜 𝗦 𝗧 𝗔 𝗛𝗢𝗧 🔞👑}\n\n"
@@ -147,60 +132,69 @@ async def enviar_mensagem_programada(bot):
     buttons = []  # Lista para armazenar os botões
 
     if not canais:
-        logger.warning("Nenhum canal encontrado na base de dados!")  # Log de alerta se não houver canais
+        logger.warning("Nenhum canal encontrado na base de dados!")
         return
 
     for canal in canais:
         canal_id = canal[0]  # ID do canal
         
         try:
-            # Buscando o nome real do canal
             chat = await bot.get_chat(canal_id)
             canal_nome = chat.title  # Agora o nome do canal será extraído corretamente
 
-            # Verificando se o canal tem um nome de usuário (isso indica que o canal é público)
             if chat.username:
-                canal_link = f"https://t.me/{chat.username}"  # Usando o nome de usuário para canais públicos
+                canal_link = f"https://t.me/{chat.username}"
             else:
-                canal_link = f"https://t.me/{canal_id}"  # Usando o ID para canais privados
+                canal_link = f"https://t.me/{canal_id}"
         except Exception as e:
             logger.error(f"Erro ao buscar o nome do canal {canal_id}: {e}")
-            canal_nome = f"Canal {canal_id}"  # Caso haja erro, use o ID como fallback
-            canal_link = f"https://t.me/{canal_id}"  # Fallback usando o ID interno
+            canal_nome = f"Canal {canal_id}"
+            canal_link = f"https://t.me/{canal_id}"
 
         buttons.append([InlineKeyboardButton(canal_nome, url=canal_link)])
 
-    # Enviando a mensagem para todos os canais cadastrados
     for canal in canais:
         canal_id = canal[0]
         try:
             await bot.send_message(chat_id=canal_id, text=mensagem, reply_markup=InlineKeyboardMarkup(buttons), parse_mode="Markdown")
-            logger.info(f"Mensagem enviada com sucesso para o canal {canal_id}")  # Log de sucesso
+            logger.info(f"Mensagem enviada com sucesso para o canal {canal_id}")
         except Exception as e:
             logger.error(f"Erro ao enviar mensagem para o canal {canal_id}: {e}")
 
-    logger.info("Mensagens enviadas para todos os canais!")  # Log para confirmar que a mensagem foi enviada para todos os canais
+    logger.info("Mensagens enviadas para todos os canais!")
 
 # Função para iniciar o bot
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logger.info("Comando /start recebido.")  # Log para verificar a execução
+    logger.info("Comando /start recebido.")
     await update.message.reply_text("Olá! Eu sou o bot e estou pronto para ajudar!")
 
-# Inicializando o agendador corretamente
-scheduler = AsyncIOScheduler()  # Agora o scheduler é inicializado corretamente
+# Função para enviar a mensagem em horário específico
+async def enviar_no_horario(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        horario = context.args[0]  # O horário será o primeiro argumento
+        hora, minuto = map(int, horario.split(":"))  # Convertendo o horário em hora e minuto
+
+        # Agendando a tarefa para enviar a mensagem no horário escolhido
+        scheduler.add_job(enviar_mensagem_programada, 'date', run_date=f"{datetime.now().date()} {hora}:{minuto}:00", args=[update.bot])
+        await update.message.reply_text(f"Mensagem agendada para {hora}:{minuto}!")
+    except Exception as e:
+        await update.message.reply_text(f"Erro ao agendar a mensagem: {e}")
+
+# Inicializando o agendador
+scheduler = AsyncIOScheduler()
 
 # Main
 async def main():
-    logger.info("Iniciando o bot...")  # Log para verificar o início da execução
+    logger.info("Iniciando o bot...")
 
-    # Configuração do bot com pool e timeout ajustados
+    # Configuração do bot
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    # Chama a função para criar a tabela 'canais' se não existir
+    # Cria a tabela 'canais' se não existir
     create_tables()
 
-    # Chama a função para adicionar todos os canais novamente
-    # adicionar_varios_canais()  # Certifique-se de ter a função 'adicionar_varios_canais' configurada corretamente
+    # Adicionando todos os canais ao banco de dados
+    adicionar_varios_canais()
 
     # Ajustando o pool de conexões e o timeout com a API pública
     app.bot._request_kwargs = {
@@ -208,27 +202,25 @@ async def main():
         'pool_size': 20  # Pool de conexões de 20
     }
 
-    # Adicionando o comando de verificação de admin
-    app.add_handler(CommandHandler("verificar_admins", verificar_admins))
+    # Adicionando os handlers
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("enviar", enviar_no_horario))  # Comando /enviar para enviar no horário escolhido
 
-    # Adicionando o comando /start
-    app.add_handler(CommandHandler("start", start))  # Comando start agora registrado
-
-    # Agendando as mensagens para horários específicos em horário de Brasília
+    # Agendando mensagens para horários específicos em horário de Brasília
     try:
         scheduler.add_job(enviar_mensagem_programada, "cron", hour=18, minute=0, args=[app.bot], timezone=brasilia_tz)  # 18h
         scheduler.add_job(enviar_mensagem_programada, "cron", hour=22, minute=0, args=[app.bot], timezone=brasilia_tz)  # 22h
         scheduler.add_job(enviar_mensagem_programada, "cron", hour=4, minute=0, args=[app.bot], timezone=brasilia_tz)   # 4h
         scheduler.add_job(enviar_mensagem_programada, "cron", hour=11, minute=0, args=[app.bot], timezone=brasilia_tz)  # 11h
-        scheduler.start()  # Iniciando o scheduler
+        scheduler.start()
     except Exception as e:
         logger.error(f"Erro ao agendar tarefa: {e}")
 
     logger.info("✅ Bot rodando com polling e agendamento diário!")
-    await app.run_polling(drop_pending_updates=True)  # Apenas polling, sem webhook
+    await app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
     try:
-        asyncio.run(main())  # Usando asyncio.run diretamente
+        asyncio.run(main())
     except Exception as e:
         logger.error(f"Erro ao iniciar o bot: {e}")
